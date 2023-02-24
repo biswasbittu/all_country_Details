804cdaa55fbc84f3d5003e5673d1d514c3cd2f03
biswasbittu <biswasbittu7415@yahoo.com>
2023-02-24 12:34:19 +0530

    cards loded from Bootstrap done

--- js/meal.js
+++ js/meal.js
@@ -1,8 +1,20 @@
 const loadMealsData=()=>{
     fetch('https://themealdb.com/api/json/v1/1/search.php?s=fish')
     .then(res=>res.json())
-    .then(data=>console.log(data));
+    .then(data=>showData(data));
+    
 
+}
+
+const showData=items=>{
+    console.log(items);
+// console.log(items.meals[0].strMeal);
+// console.log(items.meals[0].strCategory);
+// console.log(items.meals[0].strArea);
+
+for (const item of items){
+     console.log(item)
+}
 
 }
 loadMealsData();
